a43be892e0f1a3d28a5dc0379132666148399d19
Will Elliott <willscreel@gmail.com>
2018-02-14 11:58:17 -0700

    Initial commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir $HOME/.local/share/themes
+mkdir $HOME/.themes
+
+git clone https://github.com/Ubuntu/gtk-communitheme.git
+
+for file in patches/*.patch
+do
+  patch -p0 < "$file"
+done
+
+cd gtk-communitheme
+
+meson builddir --prefix=$HOME/.local
+
+ninja -C builddir install
+
+ln -s $HOME/.local/share/themes/Communitheme-MATE $HOME/.themes/Communitheme-MATE
+
+gsettings set org.mate.interface gtk-theme Communitheme-MATE
